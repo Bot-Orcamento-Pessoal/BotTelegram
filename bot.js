9461210586de4fbd95cf7e0a36a68c444078d2c9
Bot-Orcamento-Pessoal <juninhochumbinho@gmail.com>
2025-05-06 17:11:23 -0300

    Update bot.js
    Agora vai?

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.BOT_TOKEN;
-const bot = new TelegramBot(token);
+const bot = new TelegramBot(token, { webHook: true });
 const app = express();
 app.use(express.json());
 
