3fcd580df4824146c152d63ca31f9e6a7f4b3543
Bot-Orcamento-Pessoal <juninhochumbinho@gmail.com>
2025-05-21 08:18:36 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ moment.locale('pt-br');
 
 const fs = require('fs');
 const path = require('path');
-const token = '7978120569:AAFDKqGDFhCa8JUUR4Y-JL_zmYbFPBvj_0E';
+const token = '7978120569:AAH6h8ewxKbl3iAY19VgZ3MkwX9JbsVhdZ4';
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
