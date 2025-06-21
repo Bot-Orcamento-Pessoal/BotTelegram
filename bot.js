455499687af5ee1cd2b012a06f34831dd0e48faa
Bot-Orcamento-Pessoal <juninhochumbinho@gmail.com>
2025-06-21 18:57:09 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,13 +28,6 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-app.use(express.json());
-
-app.post(`/bot${token}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
-
 let saldo = 0;
 let gastos = [];
 let despesasFixas = [];
